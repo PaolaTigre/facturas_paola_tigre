06adaca66eadd0a06619009633180aeee698f81f
MiguelCriollo <macriollo.2@sudamericano.edu.ec>
2022-12-14 09:13:12 -0500

    first commit

--- src/main/kotlin/com/example/proyectdg/service/ClientService.kt
+++ src/main/kotlin/com/example/proyectdg/service/ClientService.kt
@@ -3,10 +3,15 @@ package com.example.proyectdg.service
 import com.example.proyectdg.model.Client
 import com.example.proyectdg.repository.ClientRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Example
+import org.springframework.data.domain.ExampleMatcher
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseCookie
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
+
 
 @Service
 
@@ -18,8 +23,11 @@ class ClientService {
         return clientRepository.save(client)
     }
 
-    fun list(): List<Client> {
-        return clientRepository.findAll()
+    fun list(pageable: Pageable, client: Client): Page<Client> {
+        val matcher = ExampleMatcher.matching()
+            .withIgnoreNullValues()
+            .withMatcher(("fullname"),ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase() )
+        return clientRepository.findAll(Example.of(client,matcher),pageable)
     }
 
     fun update(client: Client): Client {
